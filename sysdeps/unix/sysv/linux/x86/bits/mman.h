3fa7f2a34afbc5ab3f47c697bfa4381a7557ea03
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2023-10-31 13:32:35 -0300

    linux: Add MMAP_ABOVE4G from Linux 6.6 to sys/mman.h
    x86 added the flag (29f890d1050fc099f) for CET enabled.
    
    Also update tst-mman-consts.py test.
    
    Conflicts:
    	sysdeps/unix/sysv/linux/tst-mman-consts.py
    	  (removed patch to keep alerting about kernel differences)
    	sysdeps/unix/sysv/linux/x86/bits/mman.h
    	  (adapted to missing __USE_MISC conditional upstream)

--- sysdeps/unix/sysv/linux/x86/bits/mman.h
+++ sysdeps/unix/sysv/linux/x86/bits/mman.h
@@ -26,6 +26,7 @@
 /* Other flags.  */
 #ifdef __USE_MISC
 # define MAP_32BIT	0x40		/* Only give out 32-bit addresses.  */
+# define MAP_ABOVE4G	0x80		/* Only map above 4GB.  */
 #endif
 
 #include <bits/mman-map-flags-generic.h>
