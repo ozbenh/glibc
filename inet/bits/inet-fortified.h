e7f44ba14452356ffd634a06915a8f88074a57cc
Aaron Merey <amerey@redhat.com>
2025-03-20 13:13:33 -0400

    Add _FORTIFY_SOURCE support for inet_pton
    commit e3a6e85d67f1a48dec3e2557a83d6ce1544a58cb
    Author: Aaron Merey <amerey@redhat.com>
    Date:   Thu Mar 20 13:13:33 2025 -0400
    
        Add _FORTIFY_SOURCE support for inet_pton
    
        Add function __inet_pton_chk which calls __chk_fail when the size of
        argument dst is too small.   inet_pton is redirected to __inet_pton_chk
        or __inet_pton_warn when _FORTIFY_SOURCE is > 0.
    
        Also add tests to debug/tst-fortify.c, update the abilist with
        __inet_pton_chk and mention inet_pton fortification in maint.texi.
    
        Co-authored-by: Frédéric Bérat <fberat@redhat.com>
        Reviewed-by: Florian Weimer <fweimer@redhat.com>
    
    Conflicts:
    	debug/Makefile (New routine added to static-only-routines
    	instead of routines)
    	debug/Versions (Not added in the backport)
    	manual/maint.texi (Not relevant for this version)
    	*/libc.abilist (Not added in the backport)
    	inet/bits/inet-fortified-decl.h (Replace __REDIRECT_FORTIFY_NTH
    	with __REDIRECT_NTH)
    	inet/bits/inet-fortified.h (removed attribute_overloadable and
    	clang specific handling)
    
    Note on the changes:
    - Since we can't modify the ABI, __inet_pton_chk has been added to
      static-only-routines and `attribute_hidden` has been added to its definition.
    - __REDIRECT_FORTIFY* macros aren't available in the current version, since
      the patch to enable foritfication on glibc itself hasn't be ported.
    - clang specific handling of foritifcation has not been ported, which means the
      following had to be removed from the patch:
      - use of __attribute_overloadable__
      - use of __fortify_clang_* macros

--- inet/bits/inet-fortified.h
+++ inet/bits/inet-fortified.h
@@ -34,4 +34,21 @@ __NTH (inet_ntop (int __af, const void * __restrict __src,
 			  __af, __src, __dst, __dst_size);
 };
 
+__fortify_function int
+__NTH (inet_pton (int __af, const char *__restrict __src,
+		  void * __restrict __dst))
+{
+  size_t sz = 0;
+  if (__af == AF_INET)
+    sz = sizeof (struct in_addr);
+  else if (__af == AF_INET6)
+    sz = sizeof (struct in6_addr);
+  else
+    return __inet_pton_alias (__af, __src, __dst);
+
+  return __glibc_fortify (inet_pton, sz, sizeof (char),
+			  __glibc_objsize (__dst),
+			  __af, __src, __dst);
+};
+
 #endif /* bits/inet-fortified.h.  */
