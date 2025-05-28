af554210541be0464902a0a352a93807cb5fa478
Unknown <unknown@redhat.com>
2025-05-28 11:21:18 +1000

    glibc-RHEL-56627-1.patch
    Partial backport (without ABI changes, using libc_nonshared.a instead)
    of:
    
    commit 21571ca0d70302909cf72707b2a7736cf12190a0
    Author: Florian Weimer <fweimer@redhat.com>
    Date:   Wed Sep 11 10:05:08 2024 +0200
    
        Linux: Add the sched_setattr and sched_getattr functions
    
        And struct sched_attr.
    
        In sysdeps/unix/sysv/linux/bits/sched.h, the hack that defines
        sched_param around the inclusion of <linux/sched/types.h> is quite
        ugly, but the definition of struct sched_param has already been
        dropped by the kernel, so there is nothing else we can do and maintain
        compatibility of <sched.h> with a wide range of kernel header
        versions.  (An alternative would involve introducing a separate header
        for this functionality, but this seems unnecessary.)
    
        The existing sched_* functions that change scheduler parameters
        are already incompatible with PTHREAD_PRIO_PROTECT mutexes, so
        there is no harm in adding more functionality in this area.
    
        The documentation mostly defers to the Linux manual pages.
    
        Reviewed-by: Carlos O'Donell <carlos@redhat.com>
    
    Conflicts:
    	sysdeps/unix/sysv/linux/Makefile
    	  (variables not sorted downstream, libc_nonshared.a
    	  integration downstream)
    	sysdeps/unix/sysv/linux/Versions
    	  (not backported)
    	sysdeps/unix/sysv/linux/*/libc.abilist
    	  (not backported)
    
    The implementation uses the syscall function, not <sysdep.h>,
    to avoid implicit TCB layout dependencies.  Such dependencies
    could happen if the system call macros use information in the
    TCB to select the way the kernel is entered.

---
+++ sysdeps/unix/sysv/linux/sched_setattr.c
@@ -0,0 +1,29 @@
+/* Setting scheduling policy and attributes.
+   Copyright (C) 2024 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <sched.h>
+#include <sysdep.h>
+#include <unistd.h>
+
+int
+attribute_hidden
+sched_setattr (pid_t pid, struct sched_attr *attr, unsigned int flags)
+{
+  /* Use the syscall function for compatibility with libc_nonshared.a.  */
+  return syscall (__NR_sched_setattr, pid, attr, flags);
+}
